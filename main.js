1a5bf7ab5512cdddf699a50b8bccb4f533566924
Kenny Mc <kenymc1@gmail.com>
2020-02-10 21:27:22 -0600

    nav work in progress

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-console.log("Battleheart");
+const tourDates = [
+  {
+    date: 'Fri May 1',
+    venue: 'Lambo Festival 2020'
+    cityState: 'Atlanta, GA. USA',
+  },
+  {
+    date: 'Mon June 10',
+    venue: ' Summer Fest 2020'
+    cityState: 'Chicago, IL. USA',
+  },
+  {
+    date: 'Wed June 24',
+    venue: 'The Rainbow Festival 2020'
+    cityState: 'Nashville, TN. USA',
+  },
+  {
+    date: 'Sun July 3',
+    venue: 'The Rain or shine Festival 2020'
+    cityState: 'Cincinnati, OH. USA',
+  },
+  {
+    date: 'Fri July 8',
+    venue: 'Dirt Fest 2020'
+    cityState: 'St Louis, MO. USA',
+  },
+  {
+    date: 'Fri July 23',
+    venue: 'Green Day Festival 2020'
+    cityState: 'Kankakee, IL. USA',
+  },
+  {
+    date: 'Fri Aug 1',
+    venue: 'Down On The Green 2020'
+    cityState: 'Atlanta, GA. USA',
+  },
+  {
+    date: 'Sun Aug 16',
+    venue: 'The Continental 2020'
+    cityState: 'Gainesville, Fl. USA',
+  },
+  {
+    date: 'Wed Aug 19',
+    venue: 'Gamble Arena 2020'
+    cityState: 'Indianapolis, ID. USA',
+  },
+];
